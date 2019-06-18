d399524b9885f33efb4383b0815f0223775b0903
Mark <mdarens@gmail.com>
2019-06-18 09:39:29 -0500

    Add go to page functionality

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import {
 	lastPage,
 	nextPage,
 	previousPage,
+	gotoPage,
 	setPerPage,
-	setData,
+	setData
 } from './actions';
 import reducer from './reducer';
 import {extractPage} from './helpers';
@@ -28,6 +29,7 @@ const usePagination = (data = [], opts) => {
 		lastPage: () => dispatch(lastPage()),
 		nextPage: () => dispatch(nextPage()),
 		previousPage: () => dispatch(previousPage()),
+		gotoPage: v => dispatch(gotoPage(v)),
 		setPerPage: v => dispatch(setPerPage(v)),
 		setData: v => dispatch(setData(v)),
 		totalPages: Math.ceil(data.length / state.perPage),
